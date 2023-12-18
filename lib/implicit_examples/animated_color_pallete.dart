905fc2e4eec0c0d0fc14c2b9368d965916c1d1d3
Zobam <upc4you@gmail.com>
2023-12-18 15:22:20 +0100

    done button animation: implicit

--- lib/implicit_examples/animated_color_pallete.dart
+++ lib/implicit_examples/animated_color_pallete.dart
@@ -10,6 +10,8 @@ class AnimatedColorPalette extends StatefulWidget {
 
 class _AnimatedColorPaletteState extends State<AnimatedColorPalette> {
   List<Color> currentPalette = generateRandomPalette();
+  double containerRadius = 100;
+  double clickCount = 0;
 
   static List<Color> generateRandomPalette() {
     final random = Random();
@@ -26,8 +28,18 @@ class _AnimatedColorPaletteState extends State<AnimatedColorPalette> {
 
   void regeneratePalette() {
     setState(() {
+      clickCount += 1;
       currentPalette = generateRandomPalette();
     });
+    if (clickCount % 2 == 0) {
+      setState(() {
+        containerRadius = 50;
+      });
+    } else {
+      setState(() {
+        containerRadius = 0;
+      });
+    }
   }
 
   @override
@@ -41,11 +53,16 @@ class _AnimatedColorPaletteState extends State<AnimatedColorPalette> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             for (Color color in currentPalette)
-              Container(
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.easeInCubic,
                 width: 100,
                 height: 100,
-                color: color,
                 margin: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: color,
+                  borderRadius: BorderRadius.circular(containerRadius),
+                ),
               ),
             ElevatedButton(
               onPressed: regeneratePalette,
