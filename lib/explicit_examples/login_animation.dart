86baf39bb091153e7287179a6111b9017f71de42
Zobam <upc4you@gmail.com>
2023-12-18 17:10:52 +0100

    done login animation

--- lib/explicit_examples/login_animation.dart
+++ lib/explicit_examples/login_animation.dart
@@ -1,7 +1,48 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-class LoginScreenAnimation extends StatelessWidget {
+class LoginScreenAnimation extends StatefulWidget {
   const LoginScreenAnimation({super.key});
+
+  @override
+  State<LoginScreenAnimation> createState() => _LoginScreenAnimationState();
+}
+
+class _LoginScreenAnimationState extends State<LoginScreenAnimation>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> logoFadeAnimation;
+  late Animation<Offset> slideTransition;
+  late Animation<double> scaleAnimation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 2500,
+      ),
+    );
+    logoFadeAnimation = Tween<double>(begin: 0, end: 1).animate(controller);
+    slideTransition =
+        Tween(begin: const Offset(-1, -1), end: Offset.zero).animate(
+      CurvedAnimation(parent: controller, curve: Curves.ease),
+    );
+    scaleAnimation = Tween<double>(begin: 0, end: 1.0).animate(
+      CurvedAnimation(parent: controller, curve: Curves.ease),
+    );
+
+    controller.forward();
+
+    logoFadeAnimation.addListener(() {
+      if (logoFadeAnimation.isCompleted) {
+        controller.reverse();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,31 +51,40 @@ class LoginScreenAnimation extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const FlutterLogo(
-              size: 100.0,
+            FadeTransition(
+              opacity: logoFadeAnimation,
+              child: const FlutterLogo(
+                size: 100.0,
+              ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                children: [
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Username',
-                    ),
+              child: SlideTransition(
+                position: slideTransition,
+                child: ScaleTransition(
+                  scale: scaleAnimation,
+                  child: Column(
+                    children: [
+                      const TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      const TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                        ),
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 16.0),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Login'),
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 16.0),
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                    ),
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 16.0),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Login'),
-                  ),
-                ],
+                ),
               ),
             ),
           ],
